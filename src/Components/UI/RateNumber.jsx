90a1c71f20852d998fa35b5c202ebb6fbd1acf11
ketevaniveshaguri <ketevaniveshaguri@gmail.com>
2023-03-23 00:36:18 +0400

    deploying

--- src/Components/UI/RateNumber.jsx
+++ src/Components/UI/RateNumber.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./RateNumber.scss";
 
 const RateNumber = function (props) {
-  console.log(props);
   return (
     <>
       <li className={`rate-number ${props.className}`} onClick={props.onClick}>
